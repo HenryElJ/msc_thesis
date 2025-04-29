c8233d9c58d212b3193f333cf4eff20a94782abb
HenryElJ <46350326+HenryElJ@users.noreply.github.com>
2025-04-29 20:16:28 +0200

    Update interface.py
    Revert back to what works in Streamlit cc

--- interface.py
+++ interface.py
@@ -16,6 +16,85 @@ from initialise import select_model, system_message, params
 
 # load_dotenv()
 
+params = {
+        "chatgpt": {
+            "model": "openai/gpt-4.1",
+            "temperature": 1,
+            "top_p": 1,
+            "max_tokens": 800,
+        },
+        "deepseek": {
+            "model": "deepseek/DeepSeek-V3-0324",
+            "temperature": 0.8,
+            "top_p": 0.1,
+            "max_tokens": 2048,
+        },
+        "llama": {
+            "model": "meta/Llama-4-Scout-17B-16E-Instruct",
+            "temperature": 0.8,
+            "top_p": 0.1,
+            "max_tokens": 2048,
+        },
+        "mistral-large": {
+            "model": "Mistral-large-2411",
+            "temperature": 0.8,
+            "top_p": 0.1,
+            "max_tokens": 2048,
+        },
+        "microsoft": {
+            "model": "microsoft/MAI-DS-R1",
+            "temperature": None,
+            "top_p": None,
+            "max_tokens": 2048,
+        }
+    }
+
+# Custom functions
+def select_model(model_name: str):
+    if model_name == "google":
+        # https://python.langchain.com/docs/integrations/chat/google_generative_ai/
+        return ChatGoogleGenerativeAI(
+            model = "gemini-2.0-flash",
+            temperature = 0,
+            max_tokens = None,
+            timeout = None,
+            max_retries = 2)
+        
+    elif model_name == "mistral":
+        # https://python.langchain.com/docs/integrations/chat/mistralai/
+        return ChatMistralAI(
+            model = "mistral-small-latest",
+            temperature = 0,
+            max_retries = 2)
+        
+    else:
+        # https://python.langchain.com/docs/integrations/chat/azure_ai/
+        return AzureAIChatCompletionsModel(
+            model_name  = params[model_name]["model"],
+            temperature = params[model_name]["temperature"],
+            max_tokens  = params[model_name]["max_tokens"],
+            max_retries = 2)
+
+
+# System message
+def system_message(user: dict):
+    return f'''
+    You are part of an interface helping to guide human users through explanations for an artificial intelligence system. 
+
+    This system generates binary predictions on whether someone gets the covid vaccine or not.
+
+    The user works in the field of {user["domain"]}. Rating their skillset on a scale of ["None", "Limited", "Moderate", "Good", "Excellent"]:
+    Data analysis: {user["data_analysis"]}. Machine learning: {user["machine_learning"]}. Statistics: {user["statistics"]}. Healthcare: {user["healthcare"]}.
+
+    Provide comprehensive, but concise text-based explanations. Do not provide or recommend any code, unless explicity asked.
+
+    Your explanations should cater to their domain and skillset level, and be relevant to the artificial intelligence system. 
+
+    Only answer questions relating to questions about the artificial intelligence system.
+
+    Always recommend follow-up, clarifying questions the user could ask to help aid their understanding. 
+    '''
+
 with open("explanations_output.pickle", "rb") as file:
     explanations_output = pickle.load(file)
 
