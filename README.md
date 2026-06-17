a128f76cf29044abfb0eaf6055a4aebaf3597695
Henry El-Jawhari <henryeljawhari@gmail.com>
2026-06-17 09:47:11 +0200

    spring cleaning

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Conversational Explanations using LLMs on top of the XAI-QB
+# Conversational Explanations using Large Language Models on top of the eXplainable AI Question Bank
+## Freie Universität Berlin, Department of Mathematics and Computer Science
+## Center for Artificial Intelligence in Public Health Research (ZKI-PH) at Robert Koch Institute (RKI)
+### Henry Khalil El-Jawhari (5566377)
+##### February 26, 2026
 
-Using a range of large language models (Gemini, Mistral, Llama, ChatGPT, DeepSeek), we can further aid explainability to enhance user-specific understanding.
+The proliferation and widespread adoption of AI systems into our everyday lives poses a significant risk when these systems behave unexpectedly, or make decisions which unfairly target people/groups of people. AI systems need to be transparent, interpretable, and understandable, in order for them to be trustworthy. Central to achieving this is the field of explainable AI (XAI), where Large Language Models (LLMs) present a valuable opportunity as conversational explanatory agents, transforming complex topics into accessible narratives. These LLMs offer personalised learning, refining the user’s mental models, and filling in knowledge gaps.
 
-Visualisations coupled with text-based explanations alone is not enough to ensure the interpretability and understanding of an AI system (e.g. input/output data, model types and predictions, feature selection and weights, ...).
+In this work, an AI system was created as a prediction tool and explanation interface for vaccination uptake within the healthcare domain. A selection of LLMs (ChatGPT5.2, ClaudeOpus 4.6, Gemini3 Pro) were evaluated on their ability to answer questions from the extended explainable AI question bank (XAI-QB) when provided with this AI system’s source code. Using reference human answers as ground-truth labels, the LLMs were evaluated across the dimensions of accuracy, completeness, and faithfulness, which were used to formulate an overall ACF score. Furthermore, these reference human answers served as a baseline for readability, benchmarking the required level at which technical details could be presented in a comprehensible way.
 
-By creating a dialogue between the interface user and large language models - which has been configured to cater specifically to the user's domain knowledge, use-case, and level of expertise - we investigate this influence on the user's perceived acceptance and trustworthiness of explanations.
+The result of these evaluations suggests that LLMs can indeed be used as conversational explanatory agents. All LLMs performed comparably well, showcasing their ability to answer questions correctly, covering all of the required topics, in a semantically similar way to reference human answers. ChatGPT5.2 performed the most consistently, ranking first in accuracy and faithfulness. ClaudeOpus 4.6 provided the most complete responses, whilst Gemini3 Pro scored the best for readability. This showcases the relative strengths of different LLMs, suggesting that model selection could be tailored to the use-case and communication needs of different audiences. 
 
-Furthermore, we use the extended XAI-QB as guiding principles on what, and how explanations should be provided.
+Finally, the ACF score provides insights into where the XAI-QB and AI system can be improved — highlighting questions which need further clarification, and AI system design that lacks transparency.
---
+++ streamlit_dashboard/pages/dengue.py
@@ -0,0 +1,339 @@
+# Custom imports
+from streamlit_initialise import *
+from tabs import dengue_introduction, dengue_data, dengue_model
+
+# Import all files
+with open("h1n1/h1n1_explanations_output.pickle", "rb") as file:
+    explanations_output = pickle.load(file)
+
+# Initialise session states
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "responses" not in st.session_state:
+    st.session_state.responses = []
+
+if "model_selection" not in st.session_state:
+    st.session_state.model_selection = "mistral"
+
+if "button_selection" not in st.session_state:
+    st.session_state.button_selection = "mistral"
+
+if "user_skillset" not in st.session_state:
+    st.session_state.user_skillset = {}
+
+if "tab" not in st.session_state:
+    st.session_state.tab = None
+
+if "ask_ai" not in st.session_state:
+    # [introduction, data, model]
+    st.session_state.ask_ai = [False, False, False]
+
+st.logo(f"images/llm_icons/{st.session_state.button_selection}.png", size = "small")
+
+user_avatar = ":material/cognition:"; ai_avatar = f"images/llm_icons/{st.session_state.model_selection}.png"
+
+# Page configurations
+st.set_page_config(layout = "wide")
+
+screen_height = ScreenData().st_screen_data(key="screen_stats_")["innerHeight"]
+dict_padding = 220; viz_padding = 130; chat_padding = 270; response_height = screen_height - chat_padding - 105
+
+st.sidebar.markdown('''
+                    <a href = "http://localhost:8501/" target = "_self" class = "convex-link">
+                    <div style="display: flex; align-items: center; gap: 10px; max-width: fit-content; margin-inline: auto;">
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+                    <span class="material-symbols-rounded" style="font-size: 60px; color: #B2A8D3;">cognition</span>
+                    <h1 style="font-size: 2em; margin: 0; width: fit-content;">ConveX</h1>
+                    </div>
+                    </a>
+                    ''', unsafe_allow_html = True)
+
+st.sidebar.markdown('''
+                    <a href = "http://localhost:8501/" target = "_self" class = "convex-link">
+                    <div style="display: flex; align-items: center; gap: 10px; max-width: fit-content; margin-inline: auto">
+                    <center>
+                    <h3 style="font-size: 1em; margin: 0; width: fit-content;">Your personalised Conversational eXplainer</h3>
+                    </center>
+                    </div>
+                    </a>
+                    ''', unsafe_allow_html = True)
+
+st.sidebar.divider()
+
+llm_name_mapping = {"google"          : "Gemini (Google)", 
+                    "chatgpt"         : "ChatGPT (OpenAI)", 
+                    "mistral"         : "Mistral - Small",
+                    "mistral-large"   : "Mistral - Large", 
+                    "deepseek"        : "DeepSeek", 
+                    "llama"           : "Llama (Meta)", 
+                    "microsoft"       : "Microsoft (DeepSeek)"}
+
+button = True
+if button:
+    def button_selection(name):
+        st.session_state.button_selection = name
+
+    with st.sidebar.expander("Select the LLM to use:"):
+        llm_1, llm_2, llm_3, _ = st.columns(4)
+        llm_4, llm_5, llm_6, llm_7 = st.columns(4)
+        st.markdown("""<style> .st-emotion-cache-c1nttv.eacrzsi2:focus {background-color: #000000; border-color: #FF4B4B} </style>""", unsafe_allow_html = True)
+        llm_1.button(f"![](data:image/png;base64,{llm_images[0][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[0][0]], args = [llm_images[0][0]])
+        llm_2.button(f"![](data:image/png;base64,{llm_images[1][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[1][0]], args = [llm_images[1][0]])
+        llm_3.button(f"![](data:image/png;base64,{llm_images[2][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[2][0]], args = [llm_images[2][0]])
+        llm_4.button(f"![](data:image/png;base64,{llm_images[3][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[3][0]], args = [llm_images[3][0]])
+        llm_5.button(f"![](data:image/png;base64,{llm_images[4][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[4][0]], args = [llm_images[4][0]])
+        llm_6.button(f"![](data:image/png;base64,{llm_images[5][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[5][0]], args = [llm_images[5][0]])
+        llm_7.button(f"![](data:image/png;base64,{llm_images[6][1]})", on_click = button_selection, help = llm_name_mapping[llm_images[6][0]], args = [llm_images[6][0]])
+
+        st.markdown("<h5>You can change this model anytime.</h5>", unsafe_allow_html = True)
+
+        # with stylable_container("green", css_styles = """button {background-color: #00FF00; color: black; }""",):    
+        #     test_button = st.button("Test", type = "primary")
+
+        model_selection = st.session_state.button_selection
+else:
+    model_selection = st.sidebar.selectbox(
+        label = "Select the LLM you want to use:",
+        options = ("google", "chatgpt", "mistral", "mistral-large", "deepseek", "llama", "microsoft"),
+        index = 2,
+        format_func = lambda x: llm_name_mapping[x])
+
+st.sidebar.divider()
+
+domain = st.sidebar.selectbox("What is your area of expertise/domain?", ["Data Science", "Healthcare", "Other"])
+if domain == "Other":
+    domain = st.sidebar.text_input("Please enter your domain:")
+
+st.sidebar.divider()
+st.sidebar.write(f"Please rate your skillset level:")
+slider_options      = ["None", "Limited", "Moderate", "Good", "Excellent"]
+analysis_rating     = st.sidebar.select_slider(label = "Data Analysis",     options = slider_options, value = "Moderate")
+ml_rating           = st.sidebar.select_slider(label = "Machine Learning",  options = slider_options, value = "Moderate")
+stats_rating        = st.sidebar.select_slider(label = "Statistics",        options = slider_options, value = "Moderate")
+healthcare_rating   = st.sidebar.select_slider(label = "Healthcare",        options = slider_options, value = "Moderate")
+
+st.sidebar.divider()
+st.sidebar.markdown("<center><h5>Conversational Explanations (ConveX) ©, 2025</h5></center>", unsafe_allow_html = True)
+
+user_skillset = {
+    "domain"            : domain,
+    "data_analysis"     : analysis_rating,
+    "machine_learning"  : ml_rating,
+    "statistics"        : stats_rating,
+    "healthcare"        : healthcare_rating
+    }
+
+# Model configurations
+system_message = system_message(user_skillset); system_message = "answer shortly"
+
+model = select_model(model_selection)
+
+config = {"configurable": {"thread_id": "conversational_explainer"}}
+
+# On initial start-up, model or user skillset changes
+if ("app" not in st.session_state) | (st.session_state.model_selection != model_selection) | (st.session_state.user_skillset != user_skillset):
+    
+    st.session_state.model_selection = model_selection
+
+    prompt_template = ChatPromptTemplate(
+        [
+        ("system", system_message),
+        MessagesPlaceholder(variable_name = "messages")
+        ])
+
+    def call_model(state: MessagesState):
+        prompt = prompt_template.invoke(state)
+        response = model.invoke(prompt)
+        return {"messages": [response]}
+
+    workflow = StateGraph(state_schema = MessagesState)
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
+    memory = MemorySaver()
+    
+    st.session_state.app = workflow.compile(checkpointer = memory)
+    _ = st.session_state.app.update_state(config, {"messages": st.session_state.responses})
+
+# Tab configuration
+introduction_tab, data_tab, model_tab, explanations_tab = st.tabs(["\u2001" * 8 + x for x in [":microbe: Introduction", ":bar_chart: Data", ":robot_face: Model", ":sparkles: Explanations"]])
+
+# INTRODUCTION
+
+with introduction_tab:
+
+    st.session_state.tab = "introduction"
+
+    if floating_button(":sparkles: Let's eXplain it!", type = "primary", key = st.session_state.tab + "_tab"):
+        st.session_state.ask_ai[0] = not st.session_state.ask_ai[0]
+    
+    if st.session_state.ask_ai[0]:
+        exec(f'''{st.session_state.tab}_col, {st.session_state.tab}_ai = st.columns([0.5, 0.5]);\nwith {st.session_state.tab}_col:\n\t{generate_tab(dengue_introduction)}''')
+        exec(add_chatbox_col(st.session_state.tab))
+    else:
+        exec(generate_tab(dengue_introduction))
+
+# DATA
+with data_tab:
+
+    st.session_state.tab = "data"
+    
+    if floating_button(":sparkles: Let's eXplain it!", type = "primary", key = st.session_state.tab + "_tab"):
+        st.session_state.ask_ai[1] = not st.session_state.ask_ai[1]
+    
+    if st.session_state.ask_ai[1]:
+        exec(f'''{st.session_state.tab}_col, {st.session_state.tab}_ai = st.columns([0.5, 0.5]);\nwith {st.session_state.tab}_col:\n\t{generate_tab(dengue_data)}''')
+        exec(add_chatbox_col(st.session_state.tab))
+    else:
+        exec(generate_tab(dengue_data))
+
+# MODEL
+with model_tab:
+
+    st.session_state.tab = "model"
+    if floating_button(":sparkles: Let's eXplain it!", type = "primary", key = st.session_state.tab + "_tab"):
+        st.session_state.ask_ai[2] = not st.session_state.ask_ai[2]
+
+    if st.session_state.ask_ai[2]:
+        exec(f'''{st.session_state.tab}_col, {st.session_state.tab}_ai = st.columns([0.5, 0.5]);\nwith {st.session_state.tab}_col:\n\t{generate_tab(dengue_model)}''')
+        exec(add_chatbox_col(st.session_state.tab))
+    else:
+        exec(generate_tab(dengue_model))
+
+# EXPLANATIONS
+with explanations_tab:
+        
+    st.session_state.tab = "explanations"
+
+    viz_col, chatbox_col = st.columns([0.5, 0.5])
+
+    with viz_col:
+        viz_container = st.container(height = screen_height - viz_padding, border = False)
+        with viz_container:
+            st.plotly_chart(explanations_output["roc_auc"])
+            st.plotly_chart(explanations_output["confusion_matrix"])
+            st.plotly_chart(explanations_output["feature_importance"])
+
+    with chatbox_col:
+        chat_container = st.container(height = screen_height - chat_padding)
+        with chat_container:
+            for message in st.session_state.messages:
+                with st.chat_message(name = message["name"], avatar =  message["avatar"]):
+                    st.write(message["content"])
+
+        accepted_file_types = ["jpg", "jpeg", "png"] # ["txt", "csv", "xlsx", "pdf"]
+        if query := st.chat_input("Ask me a question!", accept_file = True, file_type = accepted_file_types):
+            with chat_container:
+
+                user_avatar = ":material/cognition:"; ai_avatar = f"images/llm_icons/{model_selection}.png"
+
+                with st.chat_message(name = "user", avatar = user_avatar):
+                    st.write(query["text"])
+                    if query["files"] != []:
+                        st.image(query["files"], width = 100) 
+                    st.session_state.messages.append({"name": "user", "avatar": user_avatar, 
+                                                    "content": query["text"] + f" [{query['files'][0].name}]" if query["files"] != [] else query["text"]})
+
+            stream = True
+            with chat_container:
+                
+                with st.chat_message(name = "assistant", avatar = ai_avatar):
+
+                    message_input = [{"type": "text", "text": query["text"]}]
+                    if query["files"] != []:
+                        image_data = base64.b64encode(query["files"][0].read()).decode("utf-8")
+                        message_input += [{"type": "image", "source_type": "base64", "mime_type": "image/jpg", "data": image_data}]
+
+                    if stream:
+                        responses = st.session_state.app.stream({"messages": [HumanMessage(message_input)]}, config, stream_mode = "messages")
+                        st.write(stream_output(responses))
+
+                        st.session_state.responses = st.session_state.app.get_state(config)[0]["messages"]
+                        st.session_state.messages.append({"name": model_selection, "avatar": ai_avatar, "content": st.session_state.responses[-1].content})
+
+                        # Lorem ipsum test
+                        # responses = lorem.paragraphs(10)
+                        # st.write(responses)
+                        # st.session_state.responses = responses
+                        # st.session_state.messages.append({"name": model_selection, "avatar": ai_avatar, "content": st.session_state.responses})
+
+                    else:
+                        responses = st.session_state.app.invoke({"messages": [HumanMessage(message_input)]}, config,)["messages"]     
+                        st.write(responses[-1].content)
+
+                        st.session_state.responses = st.session_state.app.get_state(config)[0]["messages"]
+                        st.session_state.messages.append({"name": model_selection, "avatar": ai_avatar, "content": st.session_state.responses[-1].content})
+                        # st.session_state.usage_metadata = st.session_state.app.get_state(config)[3]["writes"]["model"]["usage_metadata"]
+                    
+                    # st.markdown('''<style>.stChatMessage.st-emotion-cache-4oy321.ea2tk8x0:last-child {overflow: visible !important}</style>''', unsafe_allow_html = True)
+        
+        if st.session_state.messages == []:
+            st.info("Large language models can make mistakes. Please verify information before decisions.", icon = ":material/info:")
+        else:
+            warning_col, download_col = st.columns([0.8, 0.2], vertical_alignment = "center")
+            warning_col.info("Large language models can make mistakes. Please verify information before decisions.", icon = ":material/info:")
+            
+            export_data = "\n\n".join([x["name"] + ": " + (x["content"] + f"\n\n{'=' * 100}" if x["name"] != "user" else x["content"]) for x in st.session_state.messages])
+            download_col.download_button(
+                label = "Export chat",
+                data = export_data,
+                file_name = "chat.txt",
+                icon = ":material/download:")
+
+
+#### Page Configurations
+
+# .block-container{{overflow: hidden}}
+st.markdown(f'''<style>
+            /* Overall screen padding */
+            .block-container {{padding-top: 0.5rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem}}
+            /* Sidebar padding*/
+            .st-emotion-cache-1xgtwnd {{padding-top: 1rem; padding-bottom: 0rem}}
+            /* Last message of chatbox */
+            .stChatMessage.st-emotion-cache-4oy321.ea2tk8x0:last-child {{height: {response_height}px; overflow: scroll !important; flex-direction: column}}
+            /**/
+            /*.st-emotion-cache-1ir3vnm.ea2tk8x1:last-child {{margin: 0px}}*/
+            /* Tab height */
+            .st-al.st-as.st-bh.st-bd.st-fh.st-fi.st-fj.st-fk.st-fl.st-fm.st-fn.st-fo.st-fp {{height: 0rem}}
+            /* Tab body padding */
+            .st-bd.st-c0.st-dv.st-c8.st-c6.st-c7 {{padding-top: 0rem}}
+            /* Floating button */
+            .st-emotion-cache-37cg8s.eacrzsi1 {{background-image: linear-gradient(to bottom right, #B2A8D3, #A7C4E5); border-color: white}}
+            .st-emotion-cache-37cg8s.eacrzsi1:hover {{background-image: linear-gradient(to bottom right, #A7C4E5, #B2A8D3); border-color: white}}
+            </style>''', unsafe_allow_html = True)
+
+# .st-emotion-cache-8atqhb.e1mlolmg0 {{height: 0}}
+
+# <img src="data:image/png;base64,{logo}" alt="Your Image" style="width: 100px; height: 100px;">
+
+# Sidebar configurations
+st.markdown('''<style>.convex-link {text-decoration: none !important; color: inherit !important}.convex-link:hover {color: #B2A8D3 !important}</style>''',  unsafe_allow_html = True)
+
+# Custom CSS code to make scrollbar transparent
+transparent_scrollbar_style = """
+<style>
+/* For WebKit browsers (Chrome, Safari) */
+::-webkit-scrollbar {
+    width: 10px; /* Width of the scrollbar */
+}
+
+::-webkit-scrollbar-track {
+    background: transparent; /* Transparent track */
+}
+
+::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3); /* Semi-transparent thumb */
+    border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: rgba(0, 0, 0, 0.5); /* Slightly darker transparent thumb on hover */
+}
+
+/* For Firefox */
+* {
+    scrollbar-width: thin;
+    scrollbar-color: rgba(0, 0, 0, 0.3) transparent; /* Transparent track with semi-transparent thumb */
+}
+"""
+st.markdown(transparent_scrollbar_style, unsafe_allow_html = True)
